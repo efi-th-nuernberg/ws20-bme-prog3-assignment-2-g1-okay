b495efea083bdadf39afa99ea21262238792af80
repl.it user <replituser@example.com>
2020-10-23 10:38:58 +0000

    something is wokring but still uglyoutput

--- TextFormatter.java
+++ TextFormatter.java
@@ -4,13 +4,13 @@ import java.util.Scanner;
 class TextFormatter {
   private final int maxLineLength;
 
-  private static final String text = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy " +
-          "eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et " +
-          "accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem " +
-          "ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod " +
-          "tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et " +
-          "justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est " +
-          "Lorem ipsum dolor sit amet.";
+  private static final String text = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy "
+      + "eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et "
+      + "accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem "
+      + "ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod "
+      + "tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et "
+      + "justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est "
+      + "Lorem ipsum dolor sit amet.";
 
   public static void main(String[] args) {
     TextFormatter formatter = new TextFormatter(30);
@@ -19,24 +19,27 @@ class TextFormatter {
 
   // Konstruktor
   public TextFormatter(int maxLineLength) {
-    this.maxLineLength = maxLineLength; 
+    this.maxLineLength = maxLineLength;
   }
 
   // Ausgabe
   public void print(String aText) {
-    String formattedString;
+    String formattedString = "";
 
-    for(int currentPosition = 0; currrentPosition < aText.length; currentPosition++){
-     if(currentPosition % 30) {
+    for (int currentPosition = 0; currentPosition < aText.length(); currentPosition++) {
       // Handle linebreak
-      if(atext.charAt(currentPosition) == ' ') {
-        
+      if (currentPosition % 30 == 0) {
+
+        // Split word if necessary
+        if (aText.charAt(currentPosition) != ' ') {
+          formattedString += "-";
+        }
+
+        formattedString += "\n";
       }
-     }
       formattedString += aText.charAt(currentPosition);
     }
 
-  System.out.println(formattedString);
-  
-
+    System.out.println(formattedString);
+  }
 }
